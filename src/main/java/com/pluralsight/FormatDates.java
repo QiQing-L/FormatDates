5757c549b32414108fe6ebe2a896a05184fed0de
Qi Qing <learn.qiqing@gmail.com>
2025-10-07 14:20:25 -0400

    added and print 3 format, mm/dd/yyy and month in full.

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class FormatDates {
+    public static void main(String[] args) {
+        LocalDate today = LocalDate.now();
+        DateTimeFormatter mmddyyyFormatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
+        DateTimeFormatter mFullFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        String mdyDate = today.format(mmddyyyFormatter);
+        String mFullDate = today.format(mFullFormatter);
+
+        //LocalDate Date = LocalDate.parse(today, mmddyyyFormatter);
+
+        System.out.println(mdyDate);
+        System.out.println(today);
+        System.out.println(mFullDate);
+
+    }
 }
