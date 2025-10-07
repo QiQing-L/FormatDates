f6547e73d5275bee4d449cb29a4739dd585495fd
Qi Qing <learn.qiqing@gmail.com>
2025-10-07 14:26:47 -0400

    added and print day of week and localdatetime.

--- src/main/java/com/pluralsight/FormatDates.java
+++ src/main/java/com/pluralsight/FormatDates.java
@@ -1,6 +1,7 @@
 package com.pluralsight;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class FormatDates {
@@ -10,12 +11,14 @@ public class FormatDates {
         DateTimeFormatter mFullFormatter = DateTimeFormatter.ofPattern("MMMM d, yyyy");
         String mdyDate = today.format(mmddyyyFormatter);
         String mFullDate = today.format(mFullFormatter);
+        LocalDateTime wkDayDateTime =LocalDateTime.now();
 
         //LocalDate Date = LocalDate.parse(today, mmddyyyFormatter);
 
         System.out.println(mdyDate);
         System.out.println(today);
         System.out.println(mFullDate);
+        System.out.println(wkDayDateTime.getDayOfWeek()+ ", " + wkDayDateTime);
 
     }
 }
